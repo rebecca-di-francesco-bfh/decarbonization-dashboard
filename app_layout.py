a37b8a6ba777b3a51aa257183d7b8450d95250e3
Rebecca Di Francesco <rebecca.difrancesco@bfh.ch>
2026-02-18 13:54:08 +0100

    changed layout

--- app_layout.py
+++ app_layout.py
@@ -6,7 +6,7 @@ import streamlit as st
 def set_page_config():
     st.set_page_config(
         page_title="TE–Carbon Dashboard",
-        layout="wide",
+        layout="centered",
         initial_sidebar_state="expanded"
     )
 
